b6920fad4b0e87b67671d682700693052be71b12
Claas <claas@claas.dev>
2024-04-27 20:35:34 +0200

    Implement out of order streaming with Declarative Shadow DOM

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Streaming HTML
 
-An example of how to out-of-order stream HTML in Rust using as few dependencies as possible.
+An example of how to out of HTML out of order streaming in Rust without any JavaScript using as few dependencies as possible.
+This is made possible by using declarative shadow DOM.
+For a more in depth explanation of how this works, check out this [blog post](https://lamplightdev.com/blog/2024/01/10/streaming-html-out-of-order-without-javascript/) by @lamplightdev.
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use std::convert::Infallible;
 
-use tokio::sync::mpsc::Sender;
+use tokio::sync::mpsc::{Sender, UnboundedSender};
 
 use axum::{
     response::{Html, IntoResponse},
     routing::get,
     Router,
 };
-use tokio_stream::wrappers::ReceiverStream;
+use tokio_stream::wrappers::{ReceiverStream, UnboundedReceiverStream};
 
 #[tokio::main]
 async fn main() {
@@ -22,27 +22,97 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn streamer(sender: Sender<Result<String, Infallible>>) {
-    let mut counter = 0;
-    loop {
-        counter += 1;
-        sender
-            .send(Ok(format!("Hello, World! {}", counter)))
-            .await
-            .unwrap();
-        tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
-        if counter == 10 {
-            break;
-        }
-    }
+async fn streamer(sender: UnboundedSender<Result<String, Infallible>>) {
+    // Write initial template
+    // Notes on not obvious things:
+    // - The template element has to nested within another element to attach the shadow root to
+    // - The elements to slot have to be siblings of the template element
+    // - Sibling elements next to the template will not get rendered
+    //   Meaning only elements to be slotted should be placed as siblings to the template
+    const TEMPLATE: &str = r#"
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+    <main>
+        <template shadowrootmode="open">
+            <ol>
+                <li>
+                    <slot name="slot-1">
+                        <span>Loading slot 1</span>
+                    </slot>
+                </li>
+                <li>
+                    <slot name="slot-2">
+                        <span>Loading slot 2</span>
+                    </slot>
+                </li>
+                <li>
+                    <slot name="slot-3">
+                        <span>Loading slot 3</span>
+                    </slot>
+                </li>
+                <li>
+                    <slot name="slot-4">
+                        <span>Loading slot 4</span>
+                    </slot>
+                </li>
+            </ol>
+            <form>
+              <fieldset>
+                <legend>Choose your favorite monster</legend>
+
+                <input type="radio" id="kraken" name="monster" value="K" />
+                <label for="kraken">Kraken</label><br />
+
+                <input type="radio" id="sasquatch" name="monster" value="S" />
+                <label for="sasquatch">Sasquatch</label><br />
+
+                <input type="radio" id="mothman" name="monster" value="M" />
+                <label for="mothman">Mothman</label>
+              </fieldset>
+            </form>
+        </template>
+
+    "#;
+
+    sender.send(Ok(TEMPLATE.to_string())).unwrap();
+    // Showcase out of order
+    let slot = |slot: u8| format!("<span slot=\"slot-{}\">Slot {}</span>", slot, slot);
+
+    let next = slot(3);
+    sender.send(Ok(next)).unwrap();
+    tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+
+    let next = slot(1);
+    sender.send(Ok(next)).unwrap();
+    tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+
+    let next = slot(4);
+    sender.send(Ok(next)).unwrap();
+    tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+
+    let next = slot(2);
+    sender.send(Ok(next)).unwrap();
+
+    // Close the document
+    sender
+        .send(Ok("</main></body></html>".to_string()))
+        .unwrap();
 }
 
 async fn handler() -> impl IntoResponse {
-    let (sender, receiver) = tokio::sync::mpsc::channel(8);
+    // We know the response is max size will be the HTML document size
+    // so we can use an unbounded channel
+    let (sender, receiver) = tokio::sync::mpsc::unbounded_channel();
     tokio::spawn(streamer(sender));
 
-    // let stream = ReceiverStream::from(receiver);
-    let stream = ReceiverStream::new(receiver);
+    let stream = UnboundedReceiverStream::new(receiver);
     let body = axum::body::Body::from_stream(stream);
     Html(body)
 }
